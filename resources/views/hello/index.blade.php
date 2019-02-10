82ba1f6ba1b43ad9cf1151e14f40d2d41cb104bc
cnagama2 <chikako_mkthrk@yahoo.co.jp>
2019-02-10 10:21:51 +0000

    コンポーネントの学習

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,17 @@
 @section('content')
   <P>ここが本文のコンテンツです。</P>
   <P>必要なだけ記述できます。</P>
+  
+  @component('components.message')
+    @slot('msg_title')
+    CAUTION!
+    @endslot
+    
+    @slot('msg_content')
+    これはメッセージの表示です。
+    @endslot
+  @endcomponent
+  
 @endsection
 
 @section('footer')
