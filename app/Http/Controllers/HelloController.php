e8302356a41a0d00aa57c6043889333c6702e568
cnagama2 <chikako_mkthrk@yahoo.co.jp>
2019-03-05 14:49:46 +0000

    オリジナル・バリデータ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,18 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 
 class HelloController extends Controller
 {
-    public function index(Request $request){
-      
-      return view('hello.index', ['msg'=>'フォームを入力：']);
-
-}
-
+    public function index(Request $request)
+    {
+      return view('hello.index', ['msg' =>'フォームを入力してください。']);
+    }
+  
     public function post(HelloRequest $request)
-{
-      return view('hello.index', ['msg'=>'正しく入力されました！']);
-}
+    {
+      return view('hello.index', ['msg'=>'正しく入力されました！' ]);
+  }
 }
