44600c09568eccbca3f6bb18ff72e313b6acf786
cnagama2 <chikako_mkthrk@yahoo.co.jp>
2019-03-08 14:43:25 +0000

    モデルの削除まで

--- app/Person.php
+++ app/Person.php
@@ -3,28 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
-    public function getData()
+  public function getData()
   {
    return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
   }
-  
-    public function scopeNameEqual($query, $str)
-    {
-      return $query->where('name', $str);
-    }
-    
-    public function scopeAgeGreaterThan($query, $n)
-    {
-      return $query->where('age','>=', $n);
-    }
-    
-    public function scopeAgeLessThan($query, $n)
-    { 
-      return $query->where('age', '<=', $n);
-    }
-  
-  
+
+  protected $guarded = array('id');
+    public static $rules = array(
+      'name' => 'required',
+      'mail' => 'email',
+      'age' => 'integer|min:0|max:150'
+      );
 }
+
